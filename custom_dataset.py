571fc11df834738747822511839037d0a27fac47
JH-S3D <122940791+JH-S3D@users.noreply.github.com>
2024-04-29 11:17:56 -0700

    Update custom_dataset.py

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,14 +1,13 @@
 from torch.utils.data.dataset import Dataset
 import os
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 class MultiViewDataSet(Dataset):
 
     def find_classes(self, dir):
         classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
         classes.sort()
-        class_to_idx = {classes[i]: i for i in range(len(classes))}
-
+        class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
         return classes, class_to_idx
 
     def __init__(self, root, data_type, transform=None, target_transform=None):
@@ -22,39 +21,43 @@ class MultiViewDataSet(Dataset):
         self.target_transform = target_transform
 
         # root / <label> / <train/test> / <item>_<view>.png
-        for label in os.listdir(root):  # Label
+        for label in self.classes:
             label_path = os.path.join(root, label, data_type)
-            if os.path.isdir(label_path):  # Checking if the path is valid
+            if os.path.isdir(label_path):
                 items_views = {}
                 for file in os.listdir(label_path):
-                    if file.endswith(".png"):  # Ensure it's an image file
-                        item_view = file.split('_')  # Split filename into item and view
-                        item = '_'.join(item_view[:-1])  # Item name (handle cases where item name might contain '_')
-                        view = item_view[-1]  # View (e.g., "1.png")
-                        
-                        if item not in items_views:
-                            items_views[item] = []
-                        items_views[item].append(os.path.join(label_path, file))
+                    if file.endswith(".png") and not file.startswith('.'):
+                        item_view = file.rsplit('_', 1)  # Split filename on the last underscore
+                        if len(item_view) == 2:  # Ensure the filename is correctly formatted
+                            item, view = item_view
+                            view_path = os.path.join(label_path, file)
+                            if item not in items_views:
+                                items_views[item] = []
+                            items_views[item].append(view_path)
                 
-                # Now append the collected views and labels into the lists
+                # Append the collected views and labels into the lists
                 for item, views in items_views.items():
                     self.x.append(views)
                     self.y.append(self.class_to_idx[label])
 
-    # Override to give PyTorch access to any image on the dataset
     def __getitem__(self, index):
-        orginal_views = self.x[index]
+        original_views = self.x[index]
         views = []
+        errors = []
 
-        for view in orginal_views:
-            im = Image.open(view)
-            im = im.convert('RGB')
-            if self.transform is not None:
-                im = self.transform(im)
-            views.append(im)
+        for view_path in original_views:
+            try:
+                im = Image.open(view_path).convert('RGB')
+                if self.transform is not None:
+                    im = self.transform(im)
+                views.append(im)
+            except (IOError, UnidentifiedImageError) as e:
+                errors.append((view_path, str(e)))
+
+        if errors:
+            print(f"Failed to load some images for index {index}: {errors}")
 
         return views, self.y[index]
 
-    # Override to give PyTorch size of dataset
     def __len__(self):
         return len(self.x)
