3fcb2f7aee45387fac267ff2cecbfb05f29fd4db
Denjanzi <122940791+JH-S3D@users.noreply.github.com>
2024-04-30 16:38:45 -0700

    Update mvcnn.py

--- models/mvcnn.py
+++ models/mvcnn.py
@@ -46,19 +46,19 @@ class MVCNN(nn.Module):
             nn.ConvTranspose2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.MaxUnpool2d(kernel_size=3, stride=2),
+            #nn.MaxUnpool2d(kernel_size=3, stride=2),
             nn.ConvTranspose2d(256, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.MaxUnpool2d(kernel_size=3, stride=2),
+            #nn.MaxUnpool2d(kernel_size=3, stride=2),
             nn.ConvTranspose2d(384, 192, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.MaxUnpool2d(kernel_size=3, stride=2),
+            #nn.MaxUnpool2d(kernel_size=3, stride=2),
             nn.ConvTranspose2d(192, 64, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
 
-            nn.MaxUnpool2d(kernel_size=3, stride=2),
+            #nn.MaxUnpool2d(kernel_size=3, stride=2),
             nn.ConvTranspose2d(64, 3, kernel_size=11, stride=4, padding=2)
         )
 
