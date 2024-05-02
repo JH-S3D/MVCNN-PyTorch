245aa40f00d5be9a4ca7beb4d5cb067e5d4eb803
JH-S3D <122940791+JH-S3D@users.noreply.github.com>
2024-05-02 12:13:11 -0700

    Update mvcnn.py

--- models/mvcnn.py
+++ models/mvcnn.py
@@ -44,18 +44,18 @@ class MVCNN(nn.Module):
         self.decoder = nn.Sequential(
             # Upsampling + Convolution to progressively restore dimensions
             nn.Upsample(scale_factor=2),  # Increase size
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ConvTranspose2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
             nn.Upsample(scale_factor=2),  # Further increase size
-            nn.Conv2d(256, 192, kernel_size=3, padding=1),
+            nn.ConvTranspose2d(256, 192, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
             nn.Upsample(scale_factor=2),  # Further increase size
-            nn.Conv2d(192, 64, kernel_size=5, padding=2),
+            nn.ConvTranspose2d(192, 64, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             
-            nn.Conv2d(64, 3, kernel_size=11, stride=4, padding=2),
+            nn.ConvTranspose2d(64, 3, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
         )
 
