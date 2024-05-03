4ff483aef5d9c9d3f169490751c5d8e1967d8572
Denjanzi <122940791+JH-S3D@users.noreply.github.com>
2024-05-03 11:09:53 -0700

    Update img_autoencoder.py

--- models/img_autoencoder.py
+++ models/img_autoencoder.py
@@ -9,13 +9,9 @@ class ConvAutoencoder(torch.nn.Module):
             torch.nn.Conv2d(3, 64, 3, stride=1, padding=1),  # 
             torch.nn.ReLU(True),
             torch.nn.MaxPool2d(2, stride=1),
-            torch.nn.Conv2d(64, 16, 3, stride=1, padding=1),  # b, 8, 3, 3
+            torch.nn.Conv2d(64, 256, 3, stride=1, padding=1),  # b, 8, 3, 3
             torch.nn.ReLU(True),
             torch.nn.MaxPool2d(2, stride=1),  # b, 8, 2, 2
-
-            torch.nn.Conv2d(16, 4, 3, stride=1, padding=1),  # b, 8, 3, 3
-            torch.nn.ReLU(True),
-            torch.nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
         )
 
         #self.fc_encoder = nn.Linear(16 * 222 *222, 4096)
@@ -23,11 +19,7 @@ class ConvAutoencoder(torch.nn.Module):
 
         self.decoder = torch.nn.Sequential(
             torch.nn.Upsample(scale_factor=1, mode='nearest'),
-            torch.nn.Conv2d(4, 16, 3, stride=1, padding=2),  # b, 16, 10, 10
-            torch.nn.ReLU(True),
-
-            torch.nn.Upsample(scale_factor=1, mode='nearest'),
-            torch.nn.Conv2d(16, 64, 3, stride=1, padding=1),  # b, 16, 10, 10
+            torch.nn.Conv2d(256, 64, 3, stride=1, padding=1),  # b, 16, 10, 10
             torch.nn.ReLU(True),
             torch.nn.Upsample(scale_factor=1, mode='nearest'),
             torch.nn.Conv2d(64, 3, 3, stride=1, padding=1),  # b, 8, 3, 3
