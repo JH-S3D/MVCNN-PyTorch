4a5e6c9334e409e206d20e47880ee6ab5469968f
Denjanzi <122940791+JH-S3D@users.noreply.github.com>
2024-05-03 11:02:16 -0700

    Update img_autoencoder.py

--- models/img_autoencoder.py
+++ models/img_autoencoder.py
@@ -15,14 +15,14 @@ class ConvAutoencoder(torch.nn.Module):
 
             torch.nn.Conv2d(192, 384, kernel_size=3, stride=2, padding=1),
             torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(2, stride=2)  # b, 8, 2, 2
+            torch.nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
         )
 
         #self.fc_encoder = nn.Linear(16 * 222 *222, 4096)
         #self.fc_decoder = nn.Linear(4096, 16 * 222 *222)
 
         self.decoder = torch.nn.Sequential(
-            torch.nn.Upsample(scale_factor=2, mode='nearest'),  # b, 8, 2, 2
+            torch.nn.Upsample(scale_factor=1, mode='nearest'),  # b, 8, 2, 2
             torch.nn.Conv2d(384, 192, kernel_size=3, stride=2, padding=1),
             torch.nn.ReLU(inplace=True),
 
