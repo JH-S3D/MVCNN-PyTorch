935c958a6af01682865a03cd3b3eb5ae34dd3cac
JH-S3D <122940791+JH-S3D@users.noreply.github.com>
2024-05-22 15:21:27 -0700

    Update img_autoencoder.py

--- models/img_autoencoder.py
+++ models/img_autoencoder.py
@@ -15,8 +15,8 @@ class ConvAutoencoder(torch.nn.Module):
             torch.nn.MaxPool2d(2, stride=2)  # b, 16, 28, 28
         )
 
-        self.fc_encoder = nn.Linear(16, 28, 28, 4096)
-        self.fc_decoder = nn.Linear(4096, 16, 28, 28)
+        self.fc_encoder = nn.Linear(16 * 28 * 28, 4096)
+        self.fc_decoder = nn.Linear(4096, 16 * 28 * 28)
 
         self.decoder = torch.nn.Sequential(
             torch.nn.Upsample(scale_factor=2, mode='nearest'),
@@ -36,7 +36,7 @@ class ConvAutoencoder(torch.nn.Module):
         embedding = self.fc_encoder(x)
 
         embedding = self.fc_decoder(embedding)
-        embedding = embedding.view(1, 16, 222, 222)
+        embedding = embedding.view(1, 16, 28, 28)
 
         x = self.decoder(embedding)
         #print(x.size())
