b39e3a787b5d4815774010267f621241c9287e01
Denjanzi <122940791+JH-S3D@users.noreply.github.com>
2024-05-24 11:28:40 -0700

    Update img_autoencoder.py

--- models/img_autoencoder.py
+++ models/img_autoencoder.py
@@ -1,53 +1,63 @@
 import torch
 import torch.nn as nn
 
-class ConvAutoencoder(torch.nn.Module):
+class ConvAutoencoder(nn.Module):
     def __init__(self):
         super(ConvAutoencoder, self).__init__()
 
-        self.encoder = torch.nn.Sequential(
-            # b, 3, 224, 224
-            torch.nn.Conv2d(3, 64, 3, stride=2, padding=1),  
-            torch.nn.ReLU(True),
-            torch.nn.MaxPool2d(2, stride=2),
-            torch.nn.Conv2d(64, 16, 3, stride=1, padding=1),
-            torch.nn.ReLU(True),
-            torch.nn.MaxPool2d(2, stride=2)  # b, 16, 28, 28
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=2, padding=1),  # b, 64, 112, 112
+            nn.ReLU(True),
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),  # b, 128, 56, 56
+            nn.ReLU(True),
+            nn.Conv2d(128, 256, 3, stride=2, padding=1),  # b, 256, 28, 28
+            nn.ReLU(True),
+            nn.Conv2d(256, 512, 3, stride=2, padding=1),  # b, 512, 14, 14
+            nn.ReLU(True),
+            nn.Conv2d(512, 1024, 3, stride=2, padding=1),  # b, 1024, 7, 7
+            nn.ReLU(True),
         )
 
         self.flatten = nn.Flatten()
-        self.sigmoid = nn.Sigmoid()
-        self.fc_encoder = nn.Linear(16 * 28 * 28, 1024)
-        self.fc_decoder = nn.Linear(1024, 16 * 28 * 28)
+        self.fc_encoder = nn.Sequential(
+            nn.Linear(1024 * 7 * 7, 2048),
+            nn.ReLU(True),
+            nn.Linear(2048, 1024),
+            nn.ReLU(True)
+        )
 
-        self.decoder = torch.nn.Sequential(
-            torch.nn.Upsample(scale_factor=2, mode='nearest'),
-            torch.nn.ConvTranspose2d(16, 64, 3, stride=1, padding=1),
-            torch.nn.ReLU(True),
-            torch.nn.Upsample(scale_factor=2, mode='nearest'),
-            torch.nn.ConvTranspose2d(64, 3, 3, stride=2, padding=1, output_padding=1),  # b, 3, 223, 223
-            torch.nn.Sigmoid()
+        self.fc_decoder = nn.Sequential(
+            nn.Linear(1024, 2048),
+            nn.ReLU(True),
+            nn.Linear(2048, 1024 * 7 * 7),
+            nn.ReLU(True)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(1024, 512, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
         )
 
     def forward(self, x):
         x = self.encoder(x)
-
         x = self.flatten(x)
         embedding = self.fc_encoder(x)
-        embedding = self.sigmoid(embedding)
-
-        embedding = self.fc_decoder(embedding)
-        embedding = self.sigmoid(embedding)
-        embedding = embedding.view(1, 16, 28, 28)
-
-        x = self.decoder(embedding)
+        x = self.fc_decoder(embedding)
+        x = x.view(-1, 1024, 7, 7)
+        x = self.decoder(x)
         return x
     
     def get_embedded_vector(self, x):
-        x = self.encoder(x)
-
-        x = self.flatten(x)
-        embedding = self.fc_encoder(x)
-        embedding = self.sigmoid(embedding)
-
+        with torch.no_grad():
+            x = self.encoder(x)
+            x = self.flatten(x)
+            embedding = self.fc_encoder(x)
         return embedding
